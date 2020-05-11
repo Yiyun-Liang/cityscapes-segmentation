1ed4b4b1ba6f87a3acf0467e116542191525d3e0
Yiyun Liang <y76liang@uwaterloo.ca>
2020-05-10 20:14:25 -0700

    update filelist

--- generate_filelist.py
+++ generate_filelist.py
@@ -3,21 +3,36 @@ import numpy as np
 import os
 
 
-data_dir = '/ssd/leftImg8bit_sequence/train'
+data_dir = '/Volumes/stanford/leftImg8bit_sequence_trainvaltest/leftImg8bit_sequence/train'
 
 path = []
 sequence = []
+start = []
+end = []
 for folder in os.listdir(data_dir):
 	cur = data_dir+'/'+folder
 	if os.path.isdir(cur):
-		seqs = set()
+		start_frame = 0
+		end_frame = 0
+		first = True
+		last_seq = 0
 		for image in os.listdir(cur):
 			seq = int(image[len(folder+'_'):len(folder+'_')+6])
-			seqs.add(seq)
-		for seq in seqs:
-			path.append(cur)
-			sequence.append(seq)
+			frame_id = int(image[len(folder+'_')+6+1:len(folder+'_')+6+1+6])
 
+			if first or frame_id != end_frame+1:
+				if not first:
+					path.append('/ssd/leftImg8bit_sequence/train/'+folder)
+					sequence.append(last_seq)
+					start.append(start_frame)
+					end.append(end_frame)
+				first = False
+				start_frame = frame_id
+				end_frame = frame_id
+			else:
+				end_frame = frame_id
+			last_seq = seq
+	
 
-df = pd.DataFrame({'path': path, 'sequence': sequence})
+df = pd.DataFrame({'path': path, 'sequence': sequence, 'start': start, 'end': end})
 df.to_csv("video_train_filelist.csv", sep=',', index=False)
