cfba20c72fb614970bdb411e7f2ed9c21b97dec3
Mingyi <wisclmy0611@gmail.com>
2020-05-16 12:42:57 +0800

    init colorization

---
+++ colorization/README.md
@@ -0,0 +1,10 @@
+### Colorization Task:
+
+Start from here.
+
+
+- It includes 30 frames from each sequence.
+- It includes ground truth segmentation map for the 20th frame of each sequence.
+
+Train with ```python train.py --train_dir video_train_filelist.csv --test_dir video_val_filelist.csv```
+
---
+++ colorization/models/net.py
@@ -0,0 +1,37 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TemporalNet(nn.Module):
+    def __init__(self, embedding_net, feature_size, img_len):
+        super(TemporalNet, self).__init__()
+        self.img_len = img_len
+        #self.num_classes = math.factorial(img_len)
+        # predict the central frame
+        self.num_classes = img_len
+        self.feature_size = feature_size
+
+        self.feature_extractor = embedding_net
+        self.classifier = nn.Linear(self.feature_size* self.img_len, self.num_classes)
+        nn.init.kaiming_uniform_(self.classifier.weight)
+        #self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x1, x2, x3):
+        output1 = self.feature_extractor(x1)
+        output2 = self.feature_extractor(x2)
+        output3 = self.feature_extractor(x3)
+        output1 = output1.view(output1.shape[0], -1)
+        output2 = output1.view(output2.shape[0], -1)
+        output3 = output1.view(output3.shape[0], -1)
+
+        # dim = 1: see https://github.com/xudejing/video-clip-order-prediction
+        features = torch.cat([output1, output2, output3], dim=1)
+        x = self.classifier(features)
+        #x = self.softmax(x)
+        
+        return x
+
+    def get_embedding(self, x):
+        return self.feature_extractor(x)
