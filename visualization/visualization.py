f07a8947c929e25e1c008b1b333137c1a20218ae
Mingyi <wisclmy0611@gmail.com>
2020-06-01 13:51:11 -0700

    add smoothing

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -2,16 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-from scipy.interpolate import interp1d
+
+def smooth(csv_path,weight=0.95):
+    data = pd.read_csv(filepath_or_buffer=csv_path,header=0,names=['Step','Value'],dtype={'Step':np.int,'Value':np.float})
+    scalar = data['Value'].values
+    last = scalar[0]
+    smoothed = []
+    for point in scalar:
+        smoothed_val = last * weight + (1 - weight) * point
+        smoothed.append(smoothed_val)
+        last = smoothed_val
+
+    save = pd.DataFrame({'Step':data['Step'].values,'Value':smoothed})
+    return save
+
 
 folder = 'data/test miou/'
 
 x_list = []
 losses = []
 labels = []
+
 for file in os.listdir(folder):
-    df = pd.read_csv(folder+file)
-    df.drop(['Wall time'], axis=1, inplace=True)
+    #df = pd.read_csv(folder+file)
+    df = smooth(folder+file)
     x = df['Step'].to_list()
     loss = df['Value'].to_list()
     label = file.split('.')[0]
@@ -20,15 +34,6 @@ for file in os.listdir(folder):
     labels.append(label)
 
 for x, loss, label in zip(x_list, losses, labels):
-    #from more_itertools import chunked
-    #loss = [sum(loss) / len(loss) for loss in chunked(loss, 5)]
-    #print(len(loss))
-    x = x[::3]
-    loss = loss[::3]
-    #f1 = interp1d(x, loss, kind='cubic')
-    #xnew = np.linspace(0, int(len(x)), num=100, endpoint=True)
-    #xnew = xnew*5
-    #plt.plot(xnew * 5, f1(xnew), label=label)
     plt.plot(x, loss, label=label)
 
 plt.xlabel('Epoch')
